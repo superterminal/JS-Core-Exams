607be8a5d82a8a499edde6200bb63bcf500b55ac
superfan <superfn@protonmail.com>
2019-02-12 22:45:46 +0200

    added call func

--- Exam - 10 Feb - Fundamentals/E-venetka.js
+++ Exam - 10 Feb - Fundamentals/E-venetka.js
@@ -62,3 +62,8 @@ function solve(input) {
 
     }
 }
+// solve([ { model: 'BMW', regNumber: 'B1234SM', town: 'Varna', price: 2},
+// { model: 'BMW', regNumber: 'C5959CZ', town: 'Sofia', price: 8},
+// { model: 'Tesla', regNumber: 'NIKOLA', town: 'Burgas', price: 9},
+// { model: 'BMW', regNumber: 'A3423SM', town: 'Varna', price: 3},
+// { model: 'Lada', regNumber: 'SJSCA', town: 'Sofia', price: 3} ]);
