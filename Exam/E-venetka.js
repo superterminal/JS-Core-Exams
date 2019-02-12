5baacd9c2432db76ac4f1d347f03240f7bb725d1
superfan <superfn@protonmail.com>
2019-02-12 16:21:15 +0200

    fixed E-venetka now 100/100

--- Exam/E-venetka.js
+++ Exam/E-venetka.js
@@ -16,25 +16,9 @@ function solve(input) {
             }
         });
         let townsPriceArr = Object.entries(townsPriceObj);
-        let biggestProfit = -10000;
-        for (let el of townsPriceArr) {
-           if (biggestProfit < el[1].price) {
-               biggestProfit = el[1].price;
-           }
-        }
-        let checkForRepeatingPrices = 0;    
-        townsPriceArr.forEach(el => {
-            if (el[1].price === biggestProfit) {
-                checkForRepeatingPrices++;
-            }
-        })
-        if (checkForRepeatingPrices !== 0) {
-            townsPriceArr.sort((a, b) => b[1].price - a[1].price);
-            resultTown = townsPriceArr[0];
-        } else {
-            townsPriceArr.sort((a, b) => a[0].localeCompare(b[0]));
-            resultTown = townsPriceArr[0];
-        }
+        
+        townsPriceArr.sort((a, b) => b[1].price - a[1].price || a[0].localeCompare(b[0]));
+        resultTown = townsPriceArr[0];
 
         console.log(`${townsPriceArr[0][0]} is most profitable - ${townsPriceArr[0][1].price} BGN`);
     }
@@ -50,25 +34,9 @@ function solve(input) {
         }
     })
     modelsAndVignettes = Object.entries(modelsAndVignettes);
-    let biggestProfit = -10000;
-    for (let el of modelsAndVignettes) {
-        if (biggestProfit < el[1].count) {
-            biggestProfit = el[1].count;
-        }
-    }
-    let repeatCounter = 0;
-    modelsAndVignettes.forEach(el => {
-        if (el[1].count === biggestProfit) {
-            repeatCounter++;
-        };
-    })
-    if (repeatCounter !== 0) {
-        modelsAndVignettes.sort((a, b) => b[1].price - a[1].price);
-        resultTown = modelsAndVignettes[0];
-    } else {
-        modelsAndVignettes.sort((a, b) => a[0].localeCompare(b[0]));
-        resultTown = modelsAndVignettes[0];
-    }
+
+    modelsAndVignettes.sort((a, b) => b[1].price - a[1].price || a[0].localeCompare(b[0]));
+    resultTown = modelsAndVignettes[0];
 
     console.log(`Most driven model: ${resultTown[0]}`);
     bestDrivenCar = resultTown[0];
